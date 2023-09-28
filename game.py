a12829719119091e5f1567156528b6ad5c801266
ostrocon <ostrocon@mail.gvsu.edu>
2023-09-28 16:08:25 -0400

    player and gravity

--- game.py
+++ game.py
@@ -16,9 +16,40 @@ PLAYER_VEL = 5
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
+def flip(sprites):
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+def load_sprite_sheets(dir1, dir2, width, height, direction = False):
+    path = join("assets", dir1, dir2)
+    images = [f for f in listdir(path)  if isfile(join(path, f))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheets = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheets.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheets, (0,0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+        
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+    
+    return all_sprites
+            
+
 class Player(pygame.sprite.Sprite):
 
     COLOR = (255, 0, 0)
+    GRAVITY = 1
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
+    ANIMATION_DELAY = 5
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -27,6 +58,7 @@ class Player(pygame.sprite.Sprite):
         self.mask = None
         self.direction = "left"
         self.animation_count = 0
+        self.fall_count = 0
     
     def move(self, dx, dy):
         self.rect.x += dx
@@ -45,10 +77,26 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
 
     def loop(self, fps):
+        #self.y_vel += min(1, (self.fall_count / fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
+        self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+    
+
     def draw(self, win):
-        pygame.draw.rect(win, self.COLOR, self.rect)
+       win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 def get_background(name):
@@ -72,6 +120,16 @@ def draw(window, background, bg_image, player):
 
     pygame.display.update()
 
+def handle_move(player):
+    key = pygame.key.get_pressed()
+
+    player.x_vel = 0
+
+    if key[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if key[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
 
 def main(window):
     clock  = pygame.time.Clock()
@@ -88,6 +146,9 @@ def main(window):
                 run = False
                 break
         
+        player.loop(FPS)
+        handle_move(player)
+
         draw(window, background, bg_image, player)
 
     pygame.quit()
